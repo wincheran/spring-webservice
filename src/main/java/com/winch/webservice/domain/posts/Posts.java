26df736e93aeb24be35c71b2fd2717907ed22e95
jaewon <wincheran@gmail.com>
2018-11-10 22:27:31 +0900

    /posts 호출 시 JSON 타입 데이터 전달 기능 추가

---
+++ src/main/java/com/winch/webservice/domain/posts/Posts.java
@@ -0,0 +1,39 @@
+package com.winch.webservice.domain.posts;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+/**
+ * Posts class는 실제 DB의 table과 매칭될 class.
+ */
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Entity
+public class Posts {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(length = 500, nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    private String author;
+
+    @Builder
+    public Posts(String title, String content, String author) {
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+}
--- src/main/java/com/winch/webservice/web/WebRestController.java
+++ src/main/java/com/winch/webservice/web/WebRestController.java
@@ -1,14 +1,27 @@
 package com.winch.webservice.web;
 
+import com.winch.webservice.domain.posts.PostsRepository;
+import com.winch.webservice.dto.posts.PostsSaveRequestDto;
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 // @RestController는 @ResponseBody를 모든 메소드에서 적용해준다.
 @RestController
+@AllArgsConstructor
 public class WebRestController {
+
+    private PostsRepository postsRepository;
 
     @GetMapping("/hello")
     public String hello() {
         return "HelloWorld";
+    }
+
+    @PostMapping("/posts")
+    public void savePosts(@RequestBody PostsSaveRequestDto dto) {
+        postsRepository.save(dto.toEntity());
     }
 }
