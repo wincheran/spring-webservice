da36726da05df60429b3a9faadcc411e0fa7be99
jaewon <wincheran@gmail.com>
2018-11-10 23:12:47 +0900

    생성시간/수정시간 자동화 추가

--- src/main/java/com/winch/webservice/domain/posts/Posts.java
+++ src/main/java/com/winch/webservice/domain/posts/Posts.java
@@ -1,5 +1,6 @@
 package com.winch.webservice.domain.posts;
 
+import com.winch.webservice.domain.BaseTimeEntity;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -16,7 +17,7 @@ import javax.persistence.Id;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
 @Entity
-public class Posts {
+public class Posts extends BaseTimeEntity {
 
     @Id
     @GeneratedValue
--- src/test/java/com/winch/webservice/domain/posts/PostsRepositoryTest.java
+++ src/test/java/com/winch/webservice/domain/posts/PostsRepositoryTest.java
@@ -8,10 +8,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -22,10 +24,6 @@ public class PostsRepositoryTest {
 
     @After
     public void cleanup() {
-        /**
-         이후 테스트 코드에 영향을 끼치지 않기 위해
-         테스트 메소드가 끝날때 마다 respository 전체 비우는 코드
-         **/
         postsRepository.deleteAll();
     }
 
@@ -45,5 +43,20 @@ public class PostsRepositoryTest {
         Posts posts = postsList.get(0);
         assertThat(posts.getTitle(), is("테스트 게시글"));
         assertThat(posts.getContent(), is("테스트 본문"));
+    }
+
+    @Test
+    public void BaseTimeEntity_등록() {
+        LocalDateTime now = LocalDateTime.now();
+        postsRepository.save(Posts.builder()
+                .title("테스트 게시글")
+                .content("테스트 본문")
+                .author("wincheran@gmail.com")
+                .build());
+        List<Posts> postsList = postsRepository.findAll();
+
+        Posts posts = postsList.get(0);
+        assertTrue(posts.getCreatedDate().isAfter(now));
+        assertTrue(posts.getModifiedDate().isAfter(now));
     }
 }
